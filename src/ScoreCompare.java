00b8b488f912170bb6f5ca412cf7d42e2c25dbf1
benji <ikekaveladze1@Bens-iMac.local>
2017-08-16 17:55:43 -0700

    Added test battery stuff

---
+++ src/ScoreCompare.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.util.*;
+
+public class ScoreCompare {
+    final static int tests = 20;
+    static int round = 0;
+    static int[] points = new int[]{15,12,10,9,8,7,6,5,4,3,2,1};
+    public static void main(String[] args) throws Exception {
+	ArrayList<ProgramScore> programs = new ArrayList<ProgramScore>();
+	File folder = new File("../scores/");
+	File[] files = folder.listFiles();
+	for(int i = 0;i<files.length;i++) {
+	    File f = files[i];
+	    Scanner s = new Scanner(f);
+	    ProgramScore ps  = new ProgramScore(f.getName());
+	    programs.add(ps);
+	    while(s.hasNextDouble()) {
+		ps.add(s.nextDouble());
+	    }
+	}
+ points = Arrays.copyOf(points,programs.size()); 
+for(;round<tests;round++) {
+    int[] roundPoints = Arrays.copyOf(points,points.length);
+     Collections.sort(programs);
+     for(int i = 0;i<programs.size();i++) {
+	  ProgramScore prg = programs.get(i);
+	  int ties = 0;
+	 for(int j=i+1;j<programs.size();j++) {
+	     if(prg.cur()!=programs.get(j).cur()) break;
+	     ties++;
+	 }
+   	 //System.out.println("Round "+round+": "+ties);
+	 if(ties == 0) continue;
+	 int sum = 0;
+	 for(int j = i;j<= i+ties;j++) {
+	     sum += roundPoints[j];
+	 }
+	 for(int j = i;j<= i+ties;j++) {
+	     roundPoints[j] = (int)Math.round(sum/(ties+1.0));
+	     }
+	 i+= ties;
+	 
+	 
+     }
+
+     for(int i = 0;i<programs.size();i++) {
+	 programs.get(i).gameScore += roundPoints[i];
+     }	  
+     for(int p : roundPoints) System.out.println(p);
+     System.out.println();
+	 
+     }
+Collections.sort(programs);
+     for(int i = 0;i<programs.size();i++) {
+	 ProgramScore ps = programs.get(i);
+	 System.out.println(ps.name+": "+ps.gameScore);
+ }
+
+ }   
+ 
+   
+    
+}
+
+class ProgramScore implements Comparable<ProgramScore> {
+    double[] scores;
+    String name;
+    int gameScore = 0;
+    int addIndex = 0;
+    ProgramScore(String name) {
+	this.name = name;
+	scores = new double[ScoreCompare.tests];
+    }
+    void add(double d) {
+	scores[addIndex] = d;
+	addIndex++;
+ }
+
+    double cur() {
+	return scores[ScoreCompare.round];
+ }
+
+    public int compareTo(ProgramScore other) {
+	if(ScoreCompare.round == ScoreCompare.tests) return -Double.compare(this.gameScore,other.gameScore);
+	return Double.compare(this.scores[ScoreCompare.round],other.scores[ScoreCompare.round]);
+    }
+    public String toString() {
+	return name+": "+cur();
+    }
+}
---
+++ src/TestEval.java
@@ -0,0 +1,138 @@
+import java.io.*;
+import java.util.*;
+public class TestEval {
+
+    public static double eval(final String str) {
+	return new Object() {
+	    int pos = -1, ch;
+
+	    void nextChar() {
+		ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+	    }
+
+	    boolean eat(int charToEat) {
+		while (ch == ' ') nextChar();
+		if (ch == charToEat) {
+		    nextChar();
+		    return true;
+		}
+		return false;
+	    }
+
+	    double parse() {
+		nextChar();
+		double x = parseExpression();
+		if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
+		return x;
+	    }
+
+	    // Grammar:
+	    // expression = term | expression `+` term | expression `-` term
+	    // term = factor | term `*` factor | term `/` factor
+	    // factor = `+` factor | `-` factor | `(` expression `)`
+	    //        | number | functionName factor | factor `^` factor
+
+	    double parseExpression() {
+		double x = parseTerm();
+		for (;;) {
+		    if      (eat('+')) x += parseTerm(); // addition
+		    else if (eat('-')) x -= parseTerm(); // subtraction
+		    else return x;
+		}
+	    }
+
+	    double parseTerm() {
+		double x = parseFactor();
+		for (;;) {
+		    if      (eat('*')) x *= parseFactor(); // multiplication
+		    else if (eat('/')) x /= parseFactor(); // division
+		    else return x;
+		}
+	    }
+
+	    double parseFactor() {
+		if (eat('+')) return parseFactor(); // unary plus
+		if (eat('-')) return -parseFactor(); // unary minus
+
+		double x;
+		int startPos = this.pos;
+		if (eat('(')) { // parentheses
+		    x = parseExpression();
+		    eat(')');
+		} else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+		    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+		    x = Double.parseDouble(str.substring(startPos, this.pos));
+		} else if (ch >= 'a' && ch <= 'z') { // functions
+		    while (ch >= 'a' && ch <= 'z') nextChar();
+		    String func = str.substring(startPos, this.pos);
+		    x = parseFactor();
+		    if (func.equals("sqrt")) x = Math.sqrt(x);
+		    else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
+		    else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
+		    else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
+		    else throw new RuntimeException("Unknown function: " + func);
+		} else {
+		    throw new RuntimeException("Unexpected: " + (char)ch);
+		}
+
+		if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+
+		return x;
+	    }
+	}.parse();
+    }
+
+    public static String format(int[] nums) {
+	String s = "";
+	for(int i = 0;i<nums.length;i++) {
+	    s+=nums[i];
+	    if(i < nums.length-1) s+=" ";
+	}
+	return s;
+    }
+    
+    public static void main(String[] args) throws Exception {
+	double totalScore = 0;
+	int invalid = 0;
+	int testNo = 0;
+	Scanner tests = new Scanner(new File("testFile.txt"));
+	Scanner exps = new Scanner(new File(args[0]));
+	while(tests.hasNextLine() && exps.hasNextLine()) {
+	    testNo++;
+	    int[] testDigits = new int[5];
+	    for(int i = 0;i<5;i++) {
+		testDigits[i] = tests.nextInt();
+	    }
+	    String digitString = format(testDigits);
+	    double target = tests.nextDouble();
+	    tests.nextLine();
+	    String expression = exps.nextLine();
+	    double result,score;
+	    try {
+		if(expression.contains(".")) throw new RuntimeException("Expression can't contain '.'");
+		char[] expDigits = expression.replaceAll("\\D","").toCharArray();
+                if(expDigits.length != testDigits.length) throw new RuntimeException("Didn't use exactly 1 of each number");
+                Arrays.sort(testDigits);
+                Arrays.sort(expDigits);
+                for(int d = 0;d<testDigits.length;d++) {
+                    if((int)expDigits[d]-48 != testDigits[d]) throw new RuntimeException("Didn't use exactly 1 of each number");
+                }
+                result = eval(expression);
+                score = Math.abs(result - target);
+		totalScore += score;
+		System.out.println(score);
+	    }
+	    catch(RuntimeException e) {
+		invalid++;
+		result = Double.NaN;
+		score = Double.POSITIVE_INFINITY;
+		System.err.printf("Error (Test #%d): %s %s=>%s | %s%n",testNo,digitString,target,expression,e.getMessage());
+	    }
+	    
+	    
+	}
+	
+    }
+}
+   
+
